bf2fad0bf26b1aee700c15ec4ff0ea6aa9851c23
vincent-ff-du <36100246+vincent-ff-du@users.noreply.github.com>
2018-03-08 07:06:05 +0100

    update site.mk
    Anpassungen und Korrekturen für den 2016.2 l2tp für die Instanz Moers

--- site.mk
+++ site.mk
@@ -1,8 +1,9 @@
 GLUON_SITE_PACKAGES := \
-        gluon-mesh-batman-adv-14 \
+    gluon-mesh-batman-adv-14 \
 	gluon-alfred \
-	gluon-announced \
+	gluon-respondd \
 	gluon-autoupdater \
+	gluon-neighbour-info \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
@@ -11,28 +12,37 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-tunneldigger \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-theme \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
-        gluon-luci-portconfig \
+    gluon-luci-portconfig \
 	gluon-luci-wifi-config \
-	gluon-luci-theme \
 	gluon-luci-private-wifi \
 	gluon-next-node \
 	gluon-mesh-vpn-tunneldigger \
 	gluon-radvd \
 	gluon-setup-mode \
-	gluon-status-page \
-	gluon-migrate-vpn \
 	gluon-ssid-changer \
+	gluon-migrate-vpn \
+	gluon-tunneldigger-watchdog \
+	ffho-ath9k-blackout-workaround \
+	gluon-ebtables-filter-roguenets \
+	ffac-status-page-api \
+	gluon-status-page \
 	haveged \
 	iptables \
 	iwinfo
 
-GLUON_LANGS = en de
+GLUON_REGION ?= eu
 
-DEFAULT_GLUON_RELEASE := 0.8.3
+GLUON_LANGS ?= en de
+
+#DEFAULT_GLUON_RELEASE := 0.7-$(GLUON_BRANCH)-$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.8.4
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
+
+GLUON_ATH10K_MESH ?= ibss
